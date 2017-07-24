b6a2f27fe931aedae497c951e0b2f0194411a885
andycall <dongtiancheng@baidu.com>
2017-07-24 15:16:06 +0800

    add doc

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 可视化构建系统
 ==============
 
+## 如何安装
+
+安装依赖包
+```bash
+npm install
+```
+
+启动本地开发服务
+```
+npm start
+```
+
+## 项目说明
+此项目基于`TypeScript-React-Starter`构建, 集成Typescript, React, Webpack, tslint
+扩展命令请看![README_usage.md](./README_usage.md)
+
+## 开发说明
+此项目采用typescript作为开发语言, 同时使用tslint来进行代码格式检查. 
+执行`npm start`之后, 会启动一个本地开发服务, 之后的代码开发都是会自动进行编译
+
+## 开发F&Q
+![image](./doc/QQ20170724-120015@2x.png)
+这种问题是tslint检测到你的代码有格式问题. 请按照提示,到指定的文件和行号, 修改你的代码
+
+## 目录结构说明
--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,52 +1,40 @@
-import * as React from 'react'
-import * as CodeMirror from 'react-codemirror'
-import * as NativeCodeMirror from 'codemirror'
+import * as React from 'react';
+import * as CodeMirror from 'react-codemirror';
 
-class JSONEditor extends React.Component<{}, {}> {
-    private editorRef: ReactCodeMirror.ReactCodeMirror;
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
 
+interface JSONEditorStateInterface {
+    code: string;
+}
+
+class JSONEditor extends React.Component<{}, JSONEditorStateInterface> {
     constructor() {
         super();
+
+        this.state = {
+            code: ''
+        };
     }
 
-    componentDidMount() {
-        this.editorRef.focus();
-        this.editorRef.getCodeMirror();
+    updateCode(newCode: string) {
+        this.setState({
+            code: newCode
+        })
     }
 
     render() {
         const options = {
             lineNumbers: true,
-            readOnly: false,
-            mode: "json"
+            mode: 'javascript'
         };
 
-        const codeMirrorWrapper = 'code-mirror-wrapper';
-        const hook = document.getElementById(codeMirrorWrapper);
-
-        if (!hook) {
-            return <div id={codeMirrorWrapper}></div>;
-        }
-
-        const onChange = (value: any) => console.log(value);
-        const onFocusChange = (focused: boolean) => console.log(focused);
-        const onScroll = (scrollInfo: CodeMirror.ScrollInfo) => console.log(scrollInfo.top);
-        const codeMirrorInstance = NativeCodeMirror(hook);
-
-        return (
-            <div id={codeMirrorWrapper}>
-                <CodeMirror className="test-codemirror"
-                    codeMirrorInstance={codeMirrorInstance}
-                    onChange={onChange}
-                    onFocusChange={onFocusChange}
-                    onScroll={onScroll}
-                    options={options}
-                    path="editor"
-                    ref={(r: ReactCodeMirror.ReactCodeMirror) => this.editorRef = r}
-                    value="foo bar" />
-            </div>
-        )
+        return <CodeMirror
+            value={this.state.code}
+            onChange={this.updateCode.bind(this)}
+            options={options}
+        />
     }
 }
 
-export default JSONEditor
+export default JSONEditor;
