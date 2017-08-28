eeb7aec279cc14bc5175e880ea855cbb8468de9d
andycall <dongtiancheng@baidu.com>
2017-08-28 20:47:11 +0800

    update build scripts

--- scripts/config/webpack.config.prod.js
+++ scripts/config/webpack.config.prod.js
@@ -45,7 +45,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+const cssFilename = '[name].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -65,15 +65,14 @@ module.exports = {
     // You can exclude the *.map files from the build during deployment.
     devtool: 'source-map',
     // In production, we only want to load the polyfills and the app code.
-    entry: [require.resolve('./polyfills'), paths.appIndexJs],
+    entry: [require.resolve('./polyfills'), paths.proIndexJs],
     output: {
         // The build folder.
         path: paths.appBuild,
         // Generated JS file names (with nested folders).
         // There will be one main bundle, and one file per asynchronous chunk.
         // We don't currently advertise code splitting but Webpack supports it.
-        filename: 'static/js/[name].[chunkhash:8].js',
-        chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+        filename: '[name].js',
         // We inferred the "public path" (such as / or /my-project) from homepage.
         publicPath: publicPath,
         // Point sourcemap entries to original disk location
@@ -178,7 +177,11 @@ module.exports = {
             {
                 test: /\.(ts|tsx)$/,
                 include: paths.appSrc,
-                loader: require.resolve('ts-loader')
+                use: [
+                    require.resolve('babel-loader'),
+                    require.resolve('ts-loader')
+                ]
+                // loader: require.resolve('ts-loader'),
             },
             // The notation here is somewhat confusing.
             // "postcss" loader applies autoprefixer to our CSS.
@@ -239,50 +242,11 @@ module.exports = {
         ]
     },
     plugins: [
-        // Makes some environment variables available in index.html.
-        // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
-        // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-        // In production, it will be an empty string unless you specify "homepage"
-        // in `package.json`, in which case it will be the pathname of that URL.
-        new InterpolateHtmlPlugin(env.raw),
-        // Generates an `index.html` file with the <script> injected.
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: paths.appHtml,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true
-            }
-        }),
         // Makes some environment variables available to the JS code, for example:
         // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
         // It is absolutely essential that NODE_ENV was set to production here.
         // Otherwise React will be compiled in the very slow development mode.
         new webpack.DefinePlugin(env.stringified),
-        // Minify the code.
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                // This feature has been reported as buggy a few times, such as:
-                // https://github.com/mishoo/UglifyJS2/issues/1964
-                // We'll wait with enabling it by default until it is more solid.
-                /* eslint-disable fecs-camelcase */
-                reduce_vars: false
-                /* eslint-enable fecs-camelcase */
-            },
-            output: {
-                comments: false
-            },
-            sourceMap: true
-        }),
         // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
         new ExtractTextPlugin({
             filename: cssFilename
@@ -292,33 +256,6 @@ module.exports = {
         // having to parse `index.html`.
         new ManifestPlugin({
             fileName: 'asset-manifest.json'
-        }),
-        // Generate a service worker script that will precache, and keep up to date,
-        // the HTML & assets that are part of the Webpack build.
-        new SWPrecacheWebpackPlugin({
-            // By default, a cache-busting query parameter is appended to requests
-            // used to populate the caches, to ensure the responses are fresh.
-            // If a URL is already hashed by Webpack, then there is no concern
-            // about it being stale, and the cache-busting can be skipped.
-            dontCacheBustUrlsMatching: /\.\w{8}\./,
-            filename: 'service-worker.js',
-            logger(message) {
-                if (message.indexOf('Total precache size is') === 0) {
-                    // This message occurs for every build and is a bit too noisy.
-                    return;
-                }
-            },
-            minify: true,
-            // For unknown URLs, fallback to the index page
-            navigateFallback: publicUrl + '/index.html',
-            // Ignores URLs starting from /__ (useful for Firebase):
-            // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-            navigateFallbackWhitelist: [/^(?!\/__).*/],
-            // Don't precache sourcemaps (they're large) and build asset manifest:
-            staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-            // Work around Windows path issue in SWPrecacheWebpackPlugin:
-            // https://github.com/facebookincubator/create-react-app/issues/2235
-            stripPrefix: paths.appBuild.replace(/\\/g, '/') + '/'
         }),
         // Moment.js is an extremely popular library that bundles large locale files
         // by default due to how Webpack interprets its code. This is a practical
